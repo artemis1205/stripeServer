1bf4587b896ba57651feb25ad1840b5434e17a48
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-01-24 09:46:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -90,5 +90,27 @@ app.post('/pay', async (req, res) => {
       res.json({'client_secret': paymentIntent['client_secret']})
 })
 
+app.post('/sub', async (req, res) => {
+  const {email, payment_method} = req.body;
+
+  const customer = await stripe.customers.create({
+    payment_method: payment_method,
+    email: email,
+    invoice_settings: {
+      default_payment_method: payment_method,
+    },
+  });
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ plan: 'plan_G......' }],
+    expand: ['latest_invoice.payment_intent']
+  });
+  
+  const status = subscription['latest_invoice']['payment_intent']['status'] 
+  const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
+
+  res.json({'client_secret': client_secret, 'status': status});
+})
 
 // Most of this code provided by @thinkingjoules
