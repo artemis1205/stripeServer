a8aca5f6e118a2ca4892ad76b27a06bcb0e2836a
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-01-30 13:44:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 const express = require('express');
 const Gun = require('gun');
@@ -13,7 +15,7 @@ const port = (process.env.PORT || 8080);
 const host = '0.0.0.0';
 
 function hasValidToken(msg) {
-    return msg && msg && msg.headers && msg.headers.token && msg.headers.token === 'thisIsTheTokenForReals'
+  return msg && msg && msg.headers && msg.headers.token && msg.headers.token === 'thisIsTheTokenForReals'
 }
 
 const app = express();
@@ -36,29 +38,29 @@ const server = app.listen(port, host);
 console.log(`server listening on http://${host}:${port}`);
 
 function logIn(msg) {
-    console.log(`in msg:${JSON.stringify(msg)}.........`);
+  console.log(`in msg:${JSON.stringify(msg)}.........`);
 }
 
 function logOut(msg) {
-    console.log(`out msg:${JSON.stringify(msg)}.........`);
+  console.log(`out msg:${JSON.stringify(msg)}.........`);
 }
 
 var gun = Gun({
-    web: server,
-    localStorage: false,
-    radisk: false,
-    isValid: hasValidToken
+  web: server,
+  localStorage: false,
+  radisk: false,
+  isValid: hasValidToken
 });
 
 gun._.on('in', logIn);
 gun._.on('out', logOut);
 
 function logPeers() {
-    console.log(`Peers: ${Object.keys(gun._.opt.peers).join(', ')}`);
+  console.log(`Peers: ${Object.keys(gun._.opt.peers).join(', ')}`);
 }
 
 function logData() {
-    console.log(`In Memory: ${JSON.stringify(gun._.graph)}`);
+  console.log(`In Memory: ${JSON.stringify(gun._.graph)}`);
 }
 
 setInterval(logPeers, 5000); //Log peer list every 5 secs
@@ -70,25 +72,26 @@ const view = path.join(__dirname, 'view/main.html');
 gun.on('out', { get: { '#': { '*': '' } } })
 
 app.use(express.static('view'));
-app.get('*', function(_, res) {
-    res.sendFile(view);
+app.get('*', function (_, res) {
+  res.sendFile(view);
 });
-app.post("/mail", cors(), async (req, res) => {
-	let { text } = req.body
-	const transport = nodemailer.createTransport({
-		host: "smtp.mailtrap.io",
-		port: 2525,
-		auth: {
-			user: "10a19f549ed794",
-			pass: "f574b7b147ecdc"
-		}
-	})
 
-	await transport.sendMail({
-		from: process.env.MAIL_FROM,
-		to: "test@test.com",
-		subject: "test email",
-		html: `<div className="email" style="
+app.post("/mail", async (req, res) => {
+  let { text } = req.body
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "10a19f549ed794",
+      pass: "f574b7b147ecdc"
+    }
+  })
+
+  await transport.sendMail({
+    from: process.env.MAIL_FROM,
+    to: "test@test.com",
+    subject: "test email",
+    html: `<div className="email" style="
         border: 1px solid black;
         padding: 20px;
         font-family: sans-serif;
@@ -101,13 +104,13 @@ app.post("/mail", cors(), async (req, res) => {
         <p>All the best, Darwin</p>
          </div>
     `
-	})
+  })
 })
 
 
 
 app.post('/sub1', async (req, res) => {
-  const {email, payment_method} = req.body;
+  const { email, payment_method } = req.body;
 
   const customer = await stripe.customers.create({
     payment_method: payment_method,
@@ -122,15 +125,15 @@ app.post('/sub1', async (req, res) => {
     items: [{ plan: 'price_1KLuNhIxrCLGcFjgnEpwvgFa' }],
     expand: ['latest_invoice.payment_intent']
   });
-  
-  const status = subscription['latest_invoice']['payment_intent']['status'] 
+
+  const status = subscription['latest_invoice']['payment_intent']['status']
   const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
 
-  res.json({'client_secret': client_secret, 'status': status, 'customer': customer.id, 'sub': subscription.id});
+  res.json({ 'client_secret': client_secret, 'status': status, 'customer': customer.id, 'sub': subscription.id });
 })
 
 app.post('/sub2', async (req, res) => {
-  const {email, payment_method} = req.body;
+  const { email, payment_method } = req.body;
 
   const customer = await stripe.customers.create({
     payment_method: payment_method,
@@ -145,15 +148,15 @@ app.post('/sub2', async (req, res) => {
     items: [{ plan: 'price_1KM66QIxrCLGcFjgJ4zkYiLT' }],
     expand: ['latest_invoice.payment_intent']
   });
-  
-  const status = subscription['latest_invoice']['payment_intent']['status'] 
+
+  const status = subscription['latest_invoice']['payment_intent']['status']
   const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
 
-  res.json({'client_secret': client_secret, 'status': status, 'customer': customer.id, 'sub': subscription.id});
+  res.json({ 'client_secret': client_secret, 'status': status, 'customer': customer.id, 'sub': subscription.id });
 })
 
 app.post('/sub3', async (req, res) => {
-  const {email, payment_method} = req.body;
+  const { email, payment_method } = req.body;
 
   const customer = await stripe.customers.create({
     payment_method: payment_method,
@@ -168,20 +171,19 @@ app.post('/sub3', async (req, res) => {
     items: [{ plan: 'price_1KM66sIxrCLGcFjgPvjHr9zX' }],
     expand: ['latest_invoice.payment_intent']
   });
-  
-  const status = subscription['latest_invoice']['payment_intent']['status'] 
+
+  const status = subscription['latest_invoice']['payment_intent']['status']
   const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
 
-  res.json({'client_secret': client_secret, 'status': status, 'customer': customer.id, 'sub': subscription.id});
+  res.json({ 'client_secret': client_secret, 'status': status, 'customer': customer.id, 'sub': subscription.id });
 })
 
 app.post('/cancel', async (req, res) => {
-    const {sub} = req.body;
-    
-    const cancler = await stripe.subscriptions.del(sub);
-    
-    res.json({'client_secret': cancler['client_secret']})
+  const { sub } = req.body;
+
+  const cancler = await stripe.subscriptions.del(sub);
+
+  res.json({ 'client_secret': cancler['client_secret'] })
 })
 
 // Most of this code provided by @thinkingjoules
-
