18227873822fd077ed5e3fa031ae028da3514f98
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-02-09 11:50:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('bullet-catcher')
 require("dotenv").config()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const stripe = require('stripe')(('sk_test_51KA0UmIxrCLGcFjgvXNaPk4CRvaAJ0oQQREi9yABWzGWwxyplAgQop6YyL27xhdSqLqxpImZwISbyGGZ6r456RqF00N0Zx6l6c');
+const stripe = require('stripe')(('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');
 const nodemailer = require("nodemailer")
 
 const port = (process.env.PORT || 8080);
